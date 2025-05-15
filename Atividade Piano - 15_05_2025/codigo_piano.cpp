a0c7faf999ee369c86626ac44c888352f1244e39
Letícia Alves Roth <162461539+LeticiaRoth@users.noreply.github.com>
2025-05-15 13:43:23 -0300

    Atualização codigo_piano.cpp

--- Atividade Piano - 15_05_2025/codigo_piano.cpp
+++ Atividade Piano - 15_05_2025/codigo_piano.cpp
@@ -1,4 +1,6 @@
+//Letícia Alves Roth
 //Declaração das variáveis 
+
 int p1 = 3;
 
 int p2 = 4;
@@ -18,7 +20,6 @@ int p8 = 10;
 int C,D,E,F,G,A,B,C2; //Var Pulsador
 
 int buzz = 2; //Buzzer
-
 
 
 //Variáveis que estão conectados aos botões
@@ -47,13 +48,13 @@ pinMode(p8,INPUT);
 }
 
 
-
+ 
 //Leitura Dados do botão pressionado
 void loop()
 
 {
 
-C = digitalRead(p1);
+C = digitalRead(p1);//Leitura Dados
 
 D = digitalRead(p2);
 
@@ -70,11 +71,11 @@ B = digitalRead(p7);
 C2 = digitalRead(p8);
   
   
-  
+
 //Se os botões estiverem ligados (==1), ele irá tocar
 if(C == 1){
 
-tone(buzz,523); //Reproducir nota C (Dó)
+tone(buzz,523); //Reproducir nota C (Do)
 
 }else if(D == 1){
 
@@ -102,7 +103,9 @@ tone(buzz,988); //B (Si)
 
 }else if(C2 == 1){
 
-tone(buzz,1047); //C (Dó omais agudo)
+tone(buzz,1047); //C (Do pero una octava arriba / más agudo)
+
+}else{
 
 noTone(buzz);
 
